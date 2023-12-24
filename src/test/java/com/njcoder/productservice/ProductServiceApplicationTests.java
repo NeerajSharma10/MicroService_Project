954fb0c70013b69b9789e231c220fab0f2a26a8e
NeerajSharma10 <neerajsh1012@gmail.com>
2023-12-24 18:36:45 +0530

    added test to run the api on local

--- src/test/java/com/njcoder/productservice/ProductServiceApplicationTests.java
+++ src/test/java/com/njcoder/productservice/ProductServiceApplicationTests.java
@@ -1,13 +1,52 @@
 package com.njcoder.productservice;
 
+import com.njcoder.productservice.dto.ProductRequest;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.math.BigDecimal;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@Testcontainers
+@AutoConfigureMockMvc
 class ProductServiceApplicationTests {
 
+	@Container
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:latest");
+
+	@DynamicPropertySource
+	static void setProperty(DynamicPropertyRegistry dynamicPropertyRegistry) {
+		dynamicPropertyRegistry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+	}
+
+	@Autowired
+	MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	void shouldCreateProduct() throws Exception {
+		ProductRequest productRequest = ProductRequest.builder()
+				.name("Moto 5g")
+				.description("SmartPhone")
+				.price(new BigDecimal(13000))
+				.build();
+		ObjectMapper objectMapper = new ObjectMapper();
+		String productRequestString = objectMapper.writeValueAsString(productRequest);
+		mockMvc.perform(post("/api/product")
+				.contentType("application/json")
+				.content(productRequestString)
+		).andExpect(status().isCreated());
 	}
 
 }
